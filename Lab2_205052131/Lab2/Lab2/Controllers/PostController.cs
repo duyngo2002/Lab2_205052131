6e438baa317e7d2e98c3429e958a6545148f90b3
duyngo2002 <127843536+duyngo2002@users.noreply.github.com>
2023-03-14 17:34:16 +0700

    first commit

---
+++ Lab2_205052131/Lab2/Lab2/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string UEF( string abcdef)
+        {
+            return abcdef;
+        }
+    }
+}
